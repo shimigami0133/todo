a487ad02b67b1aab7a6eb5278d31db24cb18dfba
abhiram reddy <shimigami0133@gmail.com>
2021-05-13 15:34:25 +0530

    l

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ app.use(express.static('C:/Users/abhiram reddy/Desktop/bz/static'))
 app.get('/todo',(req,res)=>{
     res.sendFile(__dirname+"/static/todo.html")
 })
-app.post('https://shimigami-todo.herokuapp.com/programs',(req,res)=>{
+app.post('/programs',(req,res)=>{
     var x=req.body;
     x.id=idx;
     idx+=1;
     data.push(x);
     res.send("done");
 })
-app.delete('https://shimigami-todo.herokuapp.com/programs/:id',(req,res)=>{
+app.delete('/programs/:id',(req,res)=>{
     console.log(req.params.id);
     data=data.filter((x)=>{return (x.id!=req.params.id)})
     res.send("done");
 })
-app.get("https://shimigami-todo.herokuapp.com/programs",(req,res)=>{
+app.get("/programs",(req,res)=>{
     res.json(data);
 })
-app.put("https://shimigami-todo.herokuapp.com/programs/:id",(req,res)=>{
+app.put("/programs/:id",(req,res)=>{
     for(x of data)
     {
         if(x.id==req.params.id)

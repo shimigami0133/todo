750b93bc3221edc7b5c1d25fb46aa0c2905e185a
abhiram reddy <shimigami0133@gmail.com>
2021-05-19 10:53:08 +0530

    dksjf

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const express=require('express')
-const app=express()
-const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://abhiram0133:abhiram1234@cluster0.avpng.mongodb.net/database1?retryWrites=true&w=majority',()=>{
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://abhiram0133:abhiram1234@cluster0.avpng.mongodb.net/database1?retryWrites=true&w=majority', () => {
     console.log("db connected")
 })
-const router=require('./route/todo_route')
+const router = require('./route/todo_route')
+app.use(cors);
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use('/programs',router)
-app.get('/todo',(req,res)=>{
-    res.sendFile(__dirname+"/static/todo.html")
+app.use(express.urlencoded({ extended: true }))
+app.use('/programs', router)
+app.get('/todo', (req, res) => {
+    res.sendFile(__dirname + "/static/todo.html")
 })
 
-var port=process.env.PORT||8000
-app.listen(port,8000,()=>{
+var port = process.env.PORT || 8000
+app.listen(port, 8000, () => {
     console.log("server is listening");
 })
